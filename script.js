456e209d9f608c4fc97a8f50e8b79a2e4104b866
Jared Caraway <jared.caraway@gmail.com>
2023-11-12 21:45:00 -0600

    "Updated event listeners in script.js"

--- script.js
+++ script.js
@@ -1,43 +1,65 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const wordStatus = {
+    const words = {
         'example': { word: 'example', mastered: false },
         'vocabulary': { word: 'vocabulary', mastered: false },
-        'flash': { word: 'flash', mastered: false },
         // ... other words
     };
 
-    let currentWordKey = Object.keys(wordStatus)[0];
+    let currentWord = 'example';
     const flashCard = document.getElementById('flashCard');
     const wordDisplay = document.getElementById('word');
     const pronounceBtn = document.getElementById('pronounceBtn');
     const gotItBtn = document.getElementById('gotItBtn');
     const needsWorkBtn = document.getElementById('needsWorkBtn');
+    const masteredList = document.getElementById('masteredList');
 
     const updateWordDisplay = () => {
-        wordDisplay.textContent = wordStatus[currentWordKey].word;
-        flashCard.style.backgroundColor = wordStatus[currentWordKey].mastered ? '#dff0d8' : '#f2dede';
+        wordDisplay.textContent = words[currentWord].word;
+        // Toggle visibility of 'got it' button based on mastered status
+        gotItBtn.style.display = words[currentWord].mastered ? 'none' : 'inline';
+    };
+
+    const updateMasteredList = () => {
+        masteredList.innerHTML = ''; // Clear the list before updating
+        Object.keys(words).forEach((key) => {
+            if (words[key].mastered) {
+                const li = document.createElement('li');
+                li.textContent = words[key].word;
+                const pronounceButton = document.createElement('button');
+                pronounceButton.textContent = '🔊';
+                pronounceButton.onclick = () => {
+                    const utterance = new SpeechSynthesisUtterance(words[key].word);
+                    speechSynthesis.speak(utterance);
+                };
+                li.appendChild(pronounceButton);
+                masteredList.appendChild(li);
+            }
+        });
     };
 
     flashCard.addEventListener('click', () => {
-        currentWordKey = Object.keys(wordStatus)[Math.floor(Math.random() * Object.keys(wordStatus).length)];
+        const keys = Object.keys(words);
+        currentWord = keys[Math.floor(Math.random() * keys.length)];
         updateWordDisplay();
     });
 
     pronounceBtn.addEventListener('click', () => {
-        const currentWord = wordDisplay.textContent;
-        const utterance = new SpeechSynthesisUtterance(currentWord);
+        const utterance = new SpeechSynthesisUtterance(words[currentWord].word);
         speechSynthesis.speak(utterance);
     });
 
     gotItBtn.addEventListener('click', () => {
-        wordStatus[currentWordKey].mastered = true;
+        words[currentWord].mastered = true;
         updateWordDisplay();
+        updateMasteredList();
     });
 
     needsWorkBtn.addEventListener('click', () => {
-        wordStatus[currentWordKey].mastered = false;
+        words[currentWord].mastered = false;
         updateWordDisplay();
+        updateMasteredList();
     });
 
     updateWordDisplay(); // Initialize the display
+    updateMasteredList(); // Initialize the mastered list
 });
